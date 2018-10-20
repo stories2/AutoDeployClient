04f87aac3761aa93dd98451968129decdb33ebd3
Kim HyunWoo <stories2@naver.com>
2018-10-20 13:58:43 +0900

    Register : check is user already subscribed

--- AutoDeployClient/PageScripts/Controllers/Subscribe/Register.js
+++ AutoDeployClient/PageScripts/Controllers/Subscribe/Register.js
@@ -1,25 +1,45 @@
 appModule.controller("RegisterController", function ($scope, $http, $mdToast, $mdSidenav, $window, AutoDeployClientService) {
     $scope.isBrowserSupportPushService = false;
+    $scope.isAlreadySubscribed = true;
     $scope.browserNavigator = $window.navigator;
+    $scope.serviceWorkerRegistration = {};
     AutoDeployClientService.printLogMessage("RegisterController", "RegisterController", "init", LOG_LEVEL_INFO);
     $scope.onInit = function () {
         AutoDeployClientService.printLogMessage("RegisterController", "onInit", "init script", LOG_LEVEL_INFO);
         if ($scope.browserNavigator && $window) {
             $scope.isBrowserSupportPushService = true;
             AutoDeployClientService.printLogMessage("RegisterController", "onInit", "browser supported", LOG_LEVEL_INFO);
+            getServiceWorkerRegisterationInfo($scope.browserNavigator);
         }
         else {
             AutoDeployClientService.printLogMessage("RegisterController", "onInit", "browser not supported", LOG_LEVEL_WARN);
         }
     };
-    $scope.btnOnClickRegister = function () {
-        $scope.browserNavigator.serviceWorker.register('/PageScripts/AppServiceWorker.js')
+    function getServiceWorkerRegisterationInfo(navigator) {
+        navigator.serviceWorker.register('/PageScripts/AppServiceWorker.js')
             .then(function (serviceWorkerRegisteration) {
-            AutoDeployClientService.printLogMessage("RegisterController", "onInit", "service worker registered: " + JSON.stringify(serviceWorkerRegisteration), LOG_LEVEL_DEBUG);
+            AutoDeployClientService.printLogMessage("RegisterController", "getServiceWorkerRegisterationInfo", "service worker registered info: " + JSON.stringify(serviceWorkerRegisteration), LOG_LEVEL_DEBUG);
+            $scope.serviceWorkerRegistration = serviceWorkerRegisteration;
+            checkIsAlreadySubscribed($scope.serviceWorkerRegistration);
         })
             .catch(function (exception) {
-            AutoDeployClientService.printLogMessage("RegisterController", "onInit", "cannot register service worker: " + JSON.stringify(exception), LOG_LEVEL_ERROR);
+            AutoDeployClientService.printLogMessage("RegisterController", "getServiceWorkerRegisterationInfo", "cannot register service worker: " + JSON.stringify(exception), LOG_LEVEL_ERROR);
         });
+    }
+    function checkIsAlreadySubscribed(serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+            var isSubscribed = !(subscription === null);
+            AutoDeployClientService.printLogMessage("RegisterController", "checkIsAlreadySubscribed", "status: " + isSubscribed, LOG_LEVEL_DEBUG);
+            if (isSubscribed) {
+                $scope.isAlreadySubscribed = true;
+            }
+            else {
+                $scope.isAlreadySubscribed = false;
+            }
+        });
+    }
+    $scope.btnOnClickRegister = function () {
     };
 });
 //# sourceMappingURL=Register.js.map
--- AutoDeployClient/PageScripts/Controllers/Subscribe/Register.ts
+++ AutoDeployClient/PageScripts/Controllers/Subscribe/Register.ts
@@ -1,7 +1,9 @@
 ﻿appModule.controller("RegisterController", function ($scope, $http, $mdToast, $mdSidenav, $window, AutoDeployClientService) {
 
     $scope.isBrowserSupportPushService = false
+    $scope.isAlreadySubscribed = true
     $scope.browserNavigator = $window.navigator
+    $scope.serviceWorkerRegistration = {}
 
     AutoDeployClientService.printLogMessage("RegisterController", "RegisterController", "init", LOG_LEVEL_INFO)
 
@@ -11,19 +13,42 @@
         if ($scope.browserNavigator && $window) {
             $scope.isBrowserSupportPushService = true
             AutoDeployClientService.printLogMessage("RegisterController", "onInit", "browser supported", LOG_LEVEL_INFO)
+
+            getServiceWorkerRegisterationInfo($scope.browserNavigator)
         }
         else {
             AutoDeployClientService.printLogMessage("RegisterController", "onInit", "browser not supported", LOG_LEVEL_WARN)
         }
     }
 
-    $scope.btnOnClickRegister = function () {
-        $scope.browserNavigator.serviceWorker.register('/PageScripts/AppServiceWorker.js')
+    function getServiceWorkerRegisterationInfo(navigator) {
+        navigator.serviceWorker.register('/PageScripts/AppServiceWorker.js')
             .then(function (serviceWorkerRegisteration) {
-                AutoDeployClientService.printLogMessage("RegisterController", "onInit", "service worker registered: " + JSON.stringify(serviceWorkerRegisteration), LOG_LEVEL_DEBUG)
+                AutoDeployClientService.printLogMessage("RegisterController", "getServiceWorkerRegisterationInfo", "service worker registered info: " + JSON.stringify(serviceWorkerRegisteration), LOG_LEVEL_DEBUG)
+                $scope.serviceWorkerRegistration = serviceWorkerRegisteration
+
+                checkIsAlreadySubscribed($scope.serviceWorkerRegistration)
             })
             .catch(function (exception) {
-                AutoDeployClientService.printLogMessage("RegisterController", "onInit", "cannot register service worker: " + JSON.stringify(exception), LOG_LEVEL_ERROR)
+                AutoDeployClientService.printLogMessage("RegisterController", "getServiceWorkerRegisterationInfo", "cannot register service worker: " + JSON.stringify(exception), LOG_LEVEL_ERROR)
             })
+    }
+
+    function checkIsAlreadySubscribed(serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+                var isSubscribed = !(subscription === null)
+                AutoDeployClientService.printLogMessage("RegisterController", "checkIsAlreadySubscribed", "status: " + isSubscribed, LOG_LEVEL_DEBUG)
+                if (isSubscribed) {
+                    $scope.isAlreadySubscribed = true
+                }
+                else {
+                    $scope.isAlreadySubscribed = false
+                }
+            })
+    }
+
+    $scope.btnOnClickRegister = function () {
+        
     }
 })
