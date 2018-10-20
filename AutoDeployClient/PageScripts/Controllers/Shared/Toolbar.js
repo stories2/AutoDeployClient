a7165ba53c143e7d83fae7a2b846b25f9349ce49
Kim HyunWoo <stories2@naver.com>
2018-10-20 13:04:46 +0900

    _BigTemplateLayout : add toolbar

---
+++ AutoDeployClient/PageScripts/Controllers/Shared/Toolbar.js
@@ -0,0 +1,5 @@
+appModule.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidenav, AutoDeployClientService) {
+    $scope.title = "개발을 멋지게 해보자";
+    AutoDeployClientService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO);
+});
+//# sourceMappingURL=Toolbar.js.map
---
+++ AutoDeployClient/PageScripts/Controllers/Shared/Toolbar.ts
@@ -0,0 +1,6 @@
+﻿appModule.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidenav, AutoDeployClientService) {
+
+    $scope.title = "개발을 멋지게 해보자"
+
+    AutoDeployClientService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO)
+})
