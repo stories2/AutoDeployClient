2feabc25b0f504cf7e6def6b58c44bafcd66abbb
Kim HyunWoo <stories2@naver.com>
2018-10-20 11:19:24 +0900

    Subscribe controller : create view and controller

--- AutoDeployClient/App_Start/BundleConfig.cs
+++ AutoDeployClient/App_Start/BundleConfig.cs
@@ -23,9 +23,12 @@ namespace AutoDeployClient
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/*.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/*.css"));
         }
     }
 }
